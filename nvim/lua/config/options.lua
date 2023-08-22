e995d6d4b1ac21565f8e13556891774bca54429c
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2023-08-22 16:08:15 +0200

    Update options config

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,5 +1,13 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-vim.opt.cursorline = false
-vim.opt.hlsearch = false
+
+-- Enable break indent
+vim.o.breakindent = true
+
+-- Overwrite defaults
+-- Highlighting
+vim.o.hlsearch = false
+vim.o.cursorline = false
+
+-- opt.wildmode = "longest:full,full" -- Command-line completion mode
