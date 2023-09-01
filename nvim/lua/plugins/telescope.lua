7c651a8e2a9608c3350b172b092cd2b068596f12
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2023-09-01 16:27:38 +0200

    Overwrite unused hotkeys

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -5,63 +5,66 @@ return {
     commit = vim.fn.has("nvim-0.9.0") == 0 and "057ee0f8783" or nil,
     cmd = "Telescope",
     version = false, -- telescope did only one release, so use HEAD for now
-    keys = {
-      -- git
-      { "<leader>gc", "<cmd>Telescope git_commits<CR>", desc = "commits" },
-      { "<leader>gs", "<cmd>Telescope git_status<CR>", desc = "status" },
-      -- search
-      { "<leader><space>", Util.telescope("buffers"), desc = "Display Buffers" },
-      { "<leader>sg", Util.telescope("live_grep"), desc = "[S]earch by [G]rep" },
-      { "<leader>sf", Util.telescope("files"), desc = "[S]earch [F]iles" },
-      { "<leader>?", Util.telescope("oldfiles"), desc = "Recent" },
-      { "<leader>sa", Util.telescope("autocommands"), desc = "[S]earch [A]uto Commands" },
-      { "<leader>/", "<cmd>Telescope current_buffer_fuzzy_find<cr>", desc = "Fuzzy Find" },
-      { "<leader>sd", "<cmd>Telescope diagnostics bufnr=0<cr>", desc = "Document diagnostics" },
-      { "<leader>sD", "<cmd>Telescope diagnostics<cr>", desc = "Workspace diagnostics" },
-      { "<leader>sh", Util.telescope("help_tags"), desc = "Help Pages" },
-      { "<leader>sH", "<cmd>Telescope highlights<cr>", desc = "Search Highlight Groups" },
-      { "<leader>sk", "<cmd>Telescope keymaps<cr>", desc = "Key Maps" },
-      { "<leader>sM", "<cmd>Telescope man_pages<cr>", desc = "Man Pages" },
-      { "<leader>sm", "<cmd>Telescope marks<cr>", desc = "Jump to Mark" },
-      { "<leader>so", "<cmd>Telescope vim_options<cr>", desc = "Options" },
-      { "<leader>sR", "<cmd>Telescope resume<cr>", desc = "Resume" },
-      {
-        "<leader>ss",
-        Util.telescope("lsp_document_symbols", {
-          symbols = {
-            "Class",
-            "Function",
-            "Method",
-            "Constructor",
-            "Interface",
-            "Module",
-            "Struct",
-            "Trait",
-            "Field",
-            "Property",
-          },
-        }),
-        desc = "Goto Symbol",
-      },
-      {
-        "<leader>sS",
-        Util.telescope("lsp_dynamic_workspace_symbols", {
-          symbols = {
-            "Class",
-            "Function",
-            "Method",
-            "Constructor",
-            "Interface",
-            "Module",
-            "Struct",
-            "Trait",
-            "Field",
-            "Property",
-          },
-        }),
-        desc = "Goto Symbol (Workspace)",
-      },
-    },
+    keys = function()
+      return {
+        -- git
+        { "<leader>gc", "<cmd>Telescope git_commits<CR>", desc = "commits" },
+        { "<leader>gs", "<cmd>Telescope git_status<CR>", desc = "status" },
+        -- search
+        { "<leader><space>", Util.telescope("buffers"), desc = "Display Buffers" },
+        { "<leader>sg", Util.telescope("live_grep"), desc = "[S]earch by [G]rep" },
+        { "<leader>sf", Util.telescope("files"), desc = "[S]earch [F]iles" },
+        { "<leader>?", Util.telescope("oldfiles"), desc = "Recent" },
+        { "<leader>sa", Util.telescope("autocommands"), desc = "[S]earch [A]uto Commands" },
+        { "<leader>/", "<cmd>Telescope current_buffer_fuzzy_find<cr>", desc = "Fuzzy Find" },
+        { "<leader>sd", "<cmd>Telescope diagnostics bufnr=0<cr>", desc = "Document diagnostics" },
+        { "<leader>sD", "<cmd>Telescope diagnostics<cr>", desc = "Workspace diagnostics" },
+        { "<leader>sh", Util.telescope("help_tags"), desc = "Help Pages" },
+        { "<leader>sH", "<cmd>Telescope highlights<cr>", desc = "Search Highlight Groups" },
+        { "<leader>sk", "<cmd>Telescope keymaps<cr>", desc = "Key Maps" },
+        { "<leader>sM", "<cmd>Telescope man_pages<cr>", desc = "Man Pages" },
+        { "<leader>sm", "<cmd>Telescope marks<cr>", desc = "Jump to Mark" },
+        { "<leader>so", "<cmd>Telescope vim_options<cr>", desc = "Options" },
+        { "<leader>sr", Util.telescope("registers"), desc = "Register" },
+        { "<leader>sR", "<cmd>Telescope resume<cr>", desc = "Resume" },
+        {
+          "<leader>ss",
+          Util.telescope("lsp_document_symbols", {
+            symbols = {
+              "Class",
+              "Function",
+              "Method",
+              "Constructor",
+              "Interface",
+              "Module",
+              "Struct",
+              "Trait",
+              "Field",
+              "Property",
+            },
+          }),
+          desc = "Goto Symbol",
+        },
+        {
+          "<leader>sS",
+          Util.telescope("lsp_dynamic_workspace_symbols", {
+            symbols = {
+              "Class",
+              "Function",
+              "Method",
+              "Constructor",
+              "Interface",
+              "Module",
+              "Struct",
+              "Trait",
+              "Field",
+              "Property",
+            },
+          }),
+          desc = "Goto Symbol (Workspace)",
+        },
+      }
+    end,
     opts = {
       defaults = {
         prompt_prefix = " ",
