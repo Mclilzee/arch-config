15f0eb136a2b525708fe3950d8e85dbf3b00d450
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2023-09-17 11:02:02 +0200

    Update keymaps

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -2,6 +2,32 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 -- Custom Keymaps
+
+-- Deleted Keys
+vim.keymap.del("n", "<A-j>")
+vim.keymap.del("n", "<A-k>")
+vim.keymap.del("i", "<A-j>")
+vim.keymap.del("i", "<A-k>")
+vim.keymap.del("v", "<A-j>")
+vim.keymap.del("v", "<A-k>")
+vim.keymap.del("n", "<leader>gg")
+vim.keymap.del("n", "<leader>`")
+vim.keymap.del("n", "<leader>gG")
+vim.keymap.del("n", "<leader>bb")
+vim.keymap.del("n", "<leader>l")
+vim.keymap.del("n", "<leader>qq")
+vim.keymap.del("n", "<leader>L")
+vim.keymap.del("n", "<leader><tab>l")
+vim.keymap.del("n", "<leader><tab>f")
+vim.keymap.del("n", "<leader><tab><tab>")
+vim.keymap.del("n", "<leader><tab>]")
+vim.keymap.del("n", "<leader><tab>d")
+vim.keymap.del("n", "<leader><tab>[")
+vim.keymap.del("n", "<leader>ft")
+vim.keymap.del("n", "<leader>fT")
+
+local Util = require("lazyvim.util")
+
 vim.keymap.set("n", "Q", "<nop>")
 vim.keymap.set("x", "<leader>p", [["_dP]], { desc = "Paste While Holding Clipboard" })
 vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]], { desc = "Delete While Holding Clipboard" })
@@ -30,23 +56,12 @@ vim.keymap.set("n", "<leader>lS", "<cmd>LspStop<CR>", { desc = "Stop" })
 vim.keymap.set("n", "<leader>lI", "<cmd>LspInstall<CR>", { desc = "Install" })
 vim.keymap.set("n", "<leader>lu", "<cmd>LspUninstall<CR>", { desc = "Uninstall" })
 
--- Deleted Keys
-vim.keymap.del("n", "<A-j>")
-vim.keymap.del("n", "<A-k>")
-vim.keymap.del("i", "<A-j>")
-vim.keymap.del("i", "<A-k>")
-vim.keymap.del("v", "<A-j>")
-vim.keymap.del("v", "<A-k>")
-vim.keymap.del("n", "<leader>gg")
-vim.keymap.del("n", "<leader>`")
-vim.keymap.del("n", "<leader>gG")
-vim.keymap.del("n", "<leader>bb")
-vim.keymap.del("n", "<leader>l")
-vim.keymap.del("n", "<leader>qq")
-vim.keymap.del("n", "<leader>L")
-vim.keymap.del("n", "<leader><tab>l")
-vim.keymap.del("n", "<leader><tab>f")
-vim.keymap.del("n", "<leader><tab><tab>")
-vim.keymap.del("n", "<leader><tab>]")
-vim.keymap.del("n", "<leader><tab>d")
-vim.keymap.del("n", "<leader><tab>[")
+-- Terminal
+local lazyterm = function()
+  Util.float_term(nil, { cwd = Util.get_root() })
+end
+
+vim.keymap.set("n", "<leader>t", lazyterm, { desc = "Terminal (root dir)" })
+vim.keymap.set("n", "<leader>T", function()
+  Util.float_term()
+end, { desc = "Terminal (cwd)" })
