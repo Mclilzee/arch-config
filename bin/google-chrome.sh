a3aa30d94a20f3c41e2cc8b1cab4d2d395d4439a
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2024-01-19 21:46:09 +0100

    Update to use flexible distro name

--- bin/google-chrome.sh
+++ bin/google-chrome.sh
@@ -8,5 +8,4 @@ else
 	echo 'Google chrome is not installed on your windows system'
 	exit 1
 fi
-
-printf 'google-chrome() {\n\tlocal file_path\n\tfile_path=$(realpath "${1}" 2> /dev/null)\n\tif [ -z "$file_path" ]; then\n\t\tfile_path="/home"\n\tfi\n\t%s "\\\\\\\\wsl.localhost\\\\Ubuntu$file_path" > /dev/null 2>&1 &\n\tdisown\n}\n' "$chrome_path" >>~/.bashrc
+printf 'google-chrome() {\n\tlocal file_path\n\tfile_path=$(realpath "${1}" 2> /dev/null)\n\tif [ -z "$file_path" ]; then\n\t\tfile_path="/home"\n\tfi\n\t%s "\\\\\\\\wsl.localhost\\\\$WSL_DISTRO_NAME$file_path" > /dev/null 2>&1 &\n\tdisown\n}\n' "$chrome_path" >>~/.bashrc
