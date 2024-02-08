139a5852cfc432bf4ddf4d9083d20e72ff53743f
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2024-02-08 13:14:32 +0100

    Add checking cpu temp to waybar bin

---
+++ waybar/bin/cpu-temp.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+for temp_dir in /sys/class/hwmon/*; do
+	[[ "$(<"${temp_dir}/name")" =~ (coretemp|fam15h_power|k10temp) ]] && {
+		temp_dirs=("$temp_dir"/temp*_input)
+		temp_dir=${temp_dirs[0]}
+		break
+	}
+done
+
+[[ -f "$temp_dir" ]] && deg="$(($(<"$temp_dir") * 100 / 10000))"
+
+deg="${deg//./}"
+
+# Format the output
+deg="${deg/${deg: -1}/}.${deg: -1}"
+echo "$deg"
