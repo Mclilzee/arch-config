5d969c5b686065150e16a768f918d506e9f8afbe
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2024-04-06 13:42:31 +0200

    Add lsp testing file

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -2,3 +2,19 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 vim.cmd("autocmd BufEnter * set formatoptions-=cro")
+
+local client = vim.lsp.start_client({
+  name = "toplsp",
+  cmd = { "node", "/home/mclilzee/repo/toplsp" },
+})
+
+if not client then
+  return
+end
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "markdown",
+  callback = function()
+    vim.lsp.buf_attach_client(0, client)
+  end,
+})
