8eebe669e1b4d80e8a959329632070f70e114e89
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2024-08-04 03:50:15 +0200

    Remove comments

--- nvim/init.lua
+++ nvim/init.lua
@@ -174,12 +174,7 @@ require('lazy').setup({
       { 'williamboman/mason.nvim', config = true },
       'williamboman/mason-lspconfig.nvim',
       'WhoIsSethDaniel/mason-tool-installer.nvim',
-
-      -- Useful status updates for LSP.
-      -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
       { 'j-hui/fidget.nvim', opts = {} },
-
-      -- Allows extra capabilities provided by nvim-cmp
       'hrsh7th/cmp-nvim-lsp',
     },
     config = function()
