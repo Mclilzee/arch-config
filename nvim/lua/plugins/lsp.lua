11bed4e32fc01e9f2d68381b058a653dfb94fe61
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2024-08-05 00:47:46 +0200

    Remove unecessary plugin

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,14 +1,4 @@
 return {
-  { 'Bilal2453/luvit-meta', lazy = true },
-  {
-    'folke/lazydev.nvim',
-    ft = 'lua',
-    opts = {
-      library = {
-        { path = 'luvit-meta/library', words = { 'vim%.uv' } },
-      },
-    },
-  },
   {
     'neovim/nvim-lspconfig',
     dependencies = {
