4b672c2906fc0f280476e7c383d54ebccb281093
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2024-08-08 15:10:03 +0200

    Update nvim lsp config

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,100 +1,82 @@
 return {
   {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        encusre_installed = {
+                "clangd",
+                "lua_ls",
+                "rust_analyzer",
+                "tsserver",
+                'bash-language-server',
+                'css-lsp',
+                'flake8',
+                'google-java-format',
+                'html-lsp',
+                'intelephense',
+                'js-debug-adapter',
+                'jsonlint',
+                'lua-language-server',
+                'prettier',
+                'shellcheck',
+                'shfmt',
+                'stylua',
+        }
+      })
+    end
+  },
+  {
     'neovim/nvim-lspconfig',
     dependencies = {
       { 'williamboman/mason.nvim', config = true },
       'williamboman/mason-lspconfig.nvim',
-      'WhoIsSethDaniel/mason-tool-installer.nvim',
-      { 'j-hui/fidget.nvim', opts = {} },
       'hrsh7th/cmp-nvim-lsp',
     },
     config = function()
-      vim.api.nvim_create_autocmd('LspAttach', {
-        group = vim.api.nvim_create_augroup('lsp-attach', { clear = true }),
-        callback = function(event)
-          local map = function(keys, func, desc)
-            vim.keymap.set('n', keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
-          end
-          map('<leader>cr', vim.lsp.buf.rename, '[R]e[n]ame')
-          map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
-          map('<leader>cd', vim.diagnostic.open_float, '[C]ode [D]iagnostic')
-          map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
-
-          -- When you move your cursor, the highlights will be cleared (the second autocommand).
-          local client = vim.lsp.get_client_by_id(event.data.client_id)
-          if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight) then
-            local highlight_augroup = vim.api.nvim_create_augroup('lsp-highlight', { clear = false })
-            vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
-              buffer = event.buf,
-              group = highlight_augroup,
-              callback = vim.lsp.buf.document_highlight,
-            })
-
-            vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
-              buffer = event.buf,
-              group = highlight_augroup,
-              callback = vim.lsp.buf.clear_references,
-            })
-
-            vim.api.nvim_create_autocmd('LspDetach', {
-              group = vim.api.nvim_create_augroup('lsp-detach', { clear = true }),
-              callback = function(event2)
-                vim.lsp.buf.clear_references()
-                vim.api.nvim_clear_autocmds { group = 'lsp-highlight', buffer = event2.buf }
-              end,
-            })
-          end
-        end,
-      })
-      local capabilities = vim.lsp.protocol.make_client_capabilities()
-      capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
-      local servers = {
-        clangd = {},
-        rust_analyzer = {},
-        tsserver = {},
-        lua_ls = {
-          settings = {
-            Lua = {
-              completion = {
-                callSnippet = 'Replace',
-              },
-            },
-          },
-        },
-      }
-
-      require('mason').setup()
-      local ensure_installed = vim.tbl_keys(servers or {})
-      vim.list_extend(ensure_installed, {
-        'stylua', -- Used to format Lua code
-        'shellcheck',
-        'shfmt',
-        'flake8',
-        'google-java-format',
-        'jsonlint',
-        'lua-language-server',
-        'shellcheck',
-        'shfmt',
-        'stylua',
-        'bash-language-server',
-        'css-lsp',
-        'jsonlint',
-        'js-debug-adapter',
-        'html-lsp',
-        'intelephense',
-        'prettier',
-      })
-      require('mason-tool-installer').setup { ensure_installed = ensure_installed }
-
-      require('mason-lspconfig').setup {
-        handlers = {
-          function(server_name)
-            local server = servers[server_name] or {}
-            server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
-            require('lspconfig')[server_name].setup(server)
-          end,
-        },
-      }
-    end,
-  },
+      local lspconfig = require("lspconfig")
+      lspconfig.lua_ls.setup({})
+      vim.keymap.set('n', '<leader>cr', vim.lsp.buf.rename, { desc = '[R]e[n]ame' })
+      vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, { desc = '[C]ode [A]ction' })
+      vim.keymap.set('n', '<leader>cd', vim.diagnostic.open_float, { desc = '[C]ode [D]iagnostic' })
+      vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, { desc = '[G]oto [D]eclaration' })
+      -- vim.api.nvim_create_autocmd('LspAttach', {
+        -- group = vim.api.nvim_create_augroup('lsp-attach', { clear = true }),
+        -- callback = function(event)
+    --
+    --       -- When you move your cursor, the highlights will be cleared (the second autocommand).
+    --       local client = vim.lsp.get_client_by_id(event.data.client_id)
+    --       if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight) then
+    --         local highlight_augroup = vim.api.nvim_create_augroup('lsp-highlight', { clear = false })
+    --         vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
+    --           buffer = event.buf,
+    --           group = highlight_augroup,
+    --           callback = vim.lsp.buf.document_highlight,
+    --         })
+    --
+    --         vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
+    --           buffer = event.buf,
+    --           group = highlight_augroup,
+    --           callback = vim.lsp.buf.clear_references,
+    --         })
+    --
+    --         vim.api.nvim_create_autocmd('LspDetach', {
+    --           group = vim.api.nvim_create_augroup('lsp-detach', { clear = true }),
+    --           callback = function(event2)
+    --             vim.lsp.buf.clear_references()
+    --             vim.api.nvim_clear_autocmds { group = 'lsp-highlight', buffer = event2.buf }
+    --           end,
+    --         })
+    --       end
+    --     end,
+    --   })
+    --   local capabilities = vim.lsp.protocol.make_client_capabilities()
+    --   capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
+    end
+  }
 }
