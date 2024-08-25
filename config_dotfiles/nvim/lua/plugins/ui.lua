7310ce8dba7ea486c8c5bbbadb2753c47ab7ffe3
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2024-08-26 01:38:11 +0200

    Update gruvbox to gruvbox-material

--- config_dotfiles/nvim/lua/plugins/ui.lua
+++ config_dotfiles/nvim/lua/plugins/ui.lua
@@ -1,20 +1,13 @@
 return {
   {
-    'ellisonleao/gruvbox.nvim',
-    opts = {
-      transparent_mode = true,
-      italic = {
-        strings = false,
-        comments = false,
-        operators = false,
-        folds = false,
-      },
-    },
-
+    'sainnhe/gruvbox-material',
+    lazy = false,
     priority = 1000,
-    init = function()
-      vim.cmd.colorscheme 'gruvbox'
-      vim.cmd.hi 'Comment gui=none'
+    config = function()
+      vim.g.gruvbox_material_enable_italic = false
+      vim.g.gruvbox_material_background = 'hard'
+      vim.g.gruvbox_material_better_performance = 1
+      vim.cmd.colorscheme 'gruvbox-material'
     end,
   },
   {
