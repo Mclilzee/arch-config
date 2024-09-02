8305d0f2ea71a54bfbdf4a32d5f86a3341860b85
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2024-09-02 21:21:40 +0200

    Update nvim theme

--- config_dotfiles/nvim/lua/plugins/ui.lua
+++ config_dotfiles/nvim/lua/plugins/ui.lua
@@ -1,22 +1,11 @@
 return {
   {
-    'sainnhe/gruvbox-material',
-    lazy = false,
-    priority = 1000,
-    config = function()
-      vim.g.gruvbox_material_enable_italic = false
-      vim.g.gruvbox_material_background = 'hard'
-      vim.g.gruvbox_material_better_performance = 1
-      vim.g.gruvbox_material_foreground = 'material'
-      vim.g.gruvbox_material_transparent_background = 1
-      vim.cmd.colorscheme 'gruvbox-material'
-    end,
-  },
-  {
     'folke/tokyonight.nvim',
-    lazy = false,
     priority = 1000,
-    opts = {},
+    init = function()
+      vim.cmd.colorscheme 'tokyonight-night'
+      vim.cmd.hi 'Comment gui=none'
+    end,
   },
   {
     'lukas-reineke/indent-blankline.nvim',
