d28331a84ccb807d08a54f2161693d72d5d50ec0
Emad Ali <70924991+Mclilzee@users.noreply.github.com>
2025-05-04 13:33:57 +0200

    Update windows command

--- config_dotfiles/nvim/lua/config/keymap.lua
+++ config_dotfiles/nvim/lua/config/keymap.lua
@@ -5,20 +5,22 @@ vim.g.have_nerd_font = true
 vim.keymap.set('n', '!', ':!')
 vim.keymap.set('n', 'Q', '<nop>')
 vim.keymap.set('n', 's', '<nop>')
-vim.keymap.set("n", "<space>", ',', { desc = 'Move Search forward' })
+vim.keymap.set('n', '<space>', ',', { desc = 'Move Search forward' })
 -- Files and windows
 vim.keymap.set('n', '<leader>e', '<cmd>Ex<CR><cmd>set relativenumber<CR>', { desc = 'Open files tree' })
-vim.keymap.set('n', '<leader>o', '<C-w>o', { desc = 'Only current window' })
+vim.keymap.set('n', '<leader>wo', '<C-w>o', { desc = 'Only current window' })
+vim.keymap.set('n', '<leader>wl', '<C-w>v', { desc = 'Split window vertically' })
+vim.keymap.set('n', '<leader>wj', '<C-w>s', { desc = 'Split window horisontally' })
 -- Use CTRL+<hjkl> to switch between windows
 vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
 vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 -- Resize windows
-vim.keymap.set("n", "<C-Up>", "<cmd>resize +2<cr>", { desc = "Increase Window Height" })
-vim.keymap.set("n", "<C-Down>", "<cmd>resize -2<cr>", { desc = "Decrease Window Height" })
-vim.keymap.set("n", "<C-Left>", "<cmd>vertical resize -2<cr>", { desc = "Decrease Window Width" })
-vim.keymap.set("n", "<C-Right>", "<cmd>vertical resize +2<cr>", { desc = "Increase Window Width" })
+vim.keymap.set('n', '<C-Up>', '<cmd>resize +2<cr>', { desc = 'Increase Window Height' })
+vim.keymap.set('n', '<C-Down>', '<cmd>resize -2<cr>', { desc = 'Decrease Window Height' })
+vim.keymap.set('n', '<C-Left>', '<cmd>vertical resize -2<cr>', { desc = 'Decrease Window Width' })
+vim.keymap.set('n', '<C-Right>', '<cmd>vertical resize +2<cr>', { desc = 'Increase Window Width' })
 -- Clipboard
 vim.keymap.set('x', '<leader>p', [["_dP]], { desc = 'Paste While Holding Clipboard' })
 vim.keymap.set({ 'n', 'v' }, '<leader>d', [["_d]], { desc = 'Delete While Holding Clipboard' })
@@ -50,8 +52,8 @@ vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagn
 -- Exit terminal mode in the builtin terminal with a shortcut
 vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 -- Extra
-vim.keymap.set("n", "<leader>m", "<cmd>messages<cr>", { desc = "Show messages" })
-vim.keymap.set("v", "<", "<gv")
-vim.keymap.set("v", ">", ">gv")
+vim.keymap.set('n', '<leader>m', '<cmd>messages<cr>', { desc = 'Show messages' })
+vim.keymap.set('v', '<', '<gv')
+vim.keymap.set('v', '>', '>gv')
 
 return {}
